acd36fee5847ece162e8d06976a642443260d1ae
timmyjinks <tysonjjenkins.github@gmail.com>
2026-07-18 18:22:23 -0600

    fix: compile regex then resuse

--- backend/util/server.go
+++ backend/util/server.go
@@ -1,21 +1,18 @@
 package util
 
 import (
-	"errors"
 	"regexp"
 )
+
+var nameRegex = regexp.MustCompile(`^[A-Za-z-]+$`)
+var envRegex = regexp.MustCompile(`\A(?:[A-Za-z_][A-Za-z0-9_]*=[^\n]*)*(?:\n[A-Za-z_][A-Za-z0-9_]*=[^\n]*)*\z`)
 
 func validateName(name string) (bool, error) {
 	if len(name) > 24 {
 		return false, nil
 	}
 
-	r, err := regexp.Compile(`^[A-Za-z-]*$`)
-	if err != nil {
-		return false, err
-	}
-
-	return r.MatchString(name), nil
+	return nameRegex.MatchString(name), nil
 }
 
 func validateEnv(env string) (bool, error) {
@@ -23,33 +20,5 @@ func validateEnv(env string) (bool, error) {
 		return true, nil
 	}
 
-	r, err := regexp.Compile(`\A(?:[A-Za-z_][A-Za-z0-9_]*=[^\n]*)*(?:\n[A-Za-z_][A-Za-z0-9_]*=[^\n]*)*\z`)
-	if err != nil {
-		return false, err
-	}
-
-	return r.MatchString(env), nil
-}
-
-func validateRequest(name, env string) error {
-	nameOk, err := validateName(name)
-	if err != nil {
-		return err
-	}
-
-	if !nameOk {
-		return errors.New("Invalid name, can only have [a-zA-Z-] and up to 24 characters")
-	}
-
-	envOk, err := validateEnv(env)
-	if err != nil {
-		return err
-	}
-
-	if !envOk {
-		return errors.New("Invalid env, must follow key=value format")
-
-	}
-
-	return nil
+	return envRegex.MatchString(env), nil
 }
